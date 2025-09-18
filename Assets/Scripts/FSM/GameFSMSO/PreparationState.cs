b1d68e8ae2fb48058762fe5ffd94c401a94b3c42
Gyb <479276132@qq.com>
2025-09-18 14:22:03 +0800

    更换序列化字典插件

---
+++ Assets/Scripts/FSM/GameFSMSO/PreparationState.cs
@@ -0,0 +1,18 @@
+using System;
+using CommonBase;
+
+namespace TacticalRPG
+{
+    [Serializable]
+    public class PreparationState : BaseState
+    {
+        public PreparationState(string stateName, FiniteStateMachine fsm) : base(stateName, fsm) { }
+
+        public override void OnStateStart() {  }
+        public override void OnStateUpdate() {  }
+        public override void OnStateEnd() {  }
+        public override void OnStateCheckTransition() {  }
+        public override void OnReset() {  }
+        public override void OnDispose() {  }
+    }
+}
